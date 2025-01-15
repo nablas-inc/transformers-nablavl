26f0f3d1b55a0444a11182125aec13cd3ba4d2b3
Maxime Alvarez <maxime.alvarez@nablas.com>
2025-01-15 23:39:46 +0900

    Better configure MoE layers

--- src/transformers/models/qwen2/modeling_qwen2.py
+++ src/transformers/models/qwen2/modeling_qwen2.py
@@ -233,7 +233,14 @@ class Qwen2DecoderLayer(nn.Module):
         super().__init__()
         self.hidden_size = config.hidden_size
         self.self_attn = Qwen2Attention(config=config, layer_idx=layer_idx)
-        self.mlp = deepspeed.moe.layer.MoE(hidden_size=config.hidden_size, expert=Qwen2MLP(config), num_experts=config.number_of_experts, ep_size=config.expert_world_size)
+        self.mlp = deepspeed.moe.layer.MoE(
+            hidden_size=config.hidden_size,
+            expert=Qwen2MLP(config),
+            num_experts=config.number_of_experts,
+            ep_size=config.expert_world_size,
+            k=2,
+            enable_expert_tensor_parallelism=True,
+        )
         self.input_layernorm = Qwen2RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         self.post_attention_layernorm = Qwen2RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         if config.sliding_window and config._attn_implementation != "flash_attention_2":
